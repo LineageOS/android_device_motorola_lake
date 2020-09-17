d895e2e797a0d9387eef2076f6a823d6bb04e939
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2020-09-16 23:30:36 -0400

    lake: Set product device to lake for all variants
    * Update build description/fingerprint to reflect above changes.
    
    Change-Id: I5704c358954032fe8e6b6bed4705d99a5222dd98

--- init/init_lake.cpp
+++ init/init_lake.cpp
@@ -82,7 +82,7 @@ void vendor_load_device_properties()
     bootsku = GetProperty("ro.boot.hardware.sku", "");
     if (bootsku == "XT1965-T") {
         /* T-Mobile REVVLRY+ */
-        property_override_device("ro.build.description", "lake_revvl_n-user 9 PCWS29.83-56-12 ec5de release-keys");
+        property_override_device("ro.build.description", "lake_revvl-user 9 PCWS29.83-56-12 ec5de release-keys");
         property_override_device("persist.vendor.radio.customer_mbns", "tmo_usa_ims_default.mbn");
         property_override_device("persist.vendor.radio.data_con_rprt", "1");
         property_override_device("persist.vendor.ims.playout_delay", "10");
@@ -90,15 +90,15 @@ void vendor_load_device_properties()
         property_override_device("persist.vendor.ims.display_delay", "40");
         for (const auto &source : ro_props_default_source_order) {
             set_ro_build_prop(source, "fingerprint", "motorola/lake_revvl/lake:9/PCWS29.83-56-12/ec5de:user/release-keys");
-            set_ro_product_prop(source, "device", "lake_n");
+            set_ro_product_prop(source, "device", "lake");
             set_ro_product_prop(source, "model", "REVVLRY+");
-            set_ro_product_prop(source, "name", "lake_revvl_n");
+            set_ro_product_prop(source, "name", "lake_revvl");
         }
     } else {
         /* moto g(7) plus (Unlocked) */
-        property_override_device("ro.build.description", "lake-user 10 QPW30.61-21 d18ed release-keys");
+        property_override_device("ro.build.description", "lake-user 10 QPW30.61-21 cb81e release-keys");
         for (const auto &source : ro_props_default_source_order) {
-            set_ro_build_prop(source, "fingerprint", "motorola/lake_retail/lake:10/QPW30.61-18/d18ed:user/release-keys");
+            set_ro_build_prop(source, "fingerprint", "motorola/lake/lake:10/QPW30.61-21/cb81e:user/release-keys");
             set_ro_product_prop(source, "device", "lake");
             set_ro_product_prop(source, "model", "moto g(7) plus");
         }
