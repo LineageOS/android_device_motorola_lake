d5116e017c366fcde481b212d1595f00b6031929
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:51:30 +0300

    lake: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -10,20 +10,33 @@ function blob_fixup() {
     case "${1}" in
         # Add uhid group for fingerprint service
         vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service-ets.rc)
+            [ "$2" = "" ] && return 0
             sed -i "s/system input/system uhid input/" "${2}"
             ;;
         # Fix camera recording
         vendor/lib/libmmcamera2_pproc_modules.so)
+            [ "$2" = "" ] && return 0
             sed -i "s/ro.product.manufacturer/ro.product.nopefacturer/" "${2}"
             ;;
         # Use VNDK 32 libutils
         vendor/lib/hw/audio.primary.sdm660.so | vendor/lib/libmotaudioutils.so | vendor/lib/libsensorndkbridge.so | vendor/lib/libtinycompress_vendor.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
         vendor/lib/soundfx/libspeakerbundle.so | vendor/lib/soundfx/libmmieffectswrapper.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # If we're being sourced by the common script that we called,
@@ -37,5 +50,6 @@ set -e
 export DEVICE=lake
 export DEVICE_COMMON=msm8998-common
 export VENDOR=motorola
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
