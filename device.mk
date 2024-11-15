9ed4fc2aec907c2ce94153fa98b1201488e0f0c0
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-15 00:27:48 -0500

    lake: Commonize libutils-v32
    Change-Id: Ic6757897e52b7783ee2f58ff84edee4ec842ced1
    (cherry picked from commit 17a17b0d113f0cba94bf2c15470e2eb4ec4df725)

--- device.mk
+++ device.mk
@@ -127,10 +127,6 @@ PRODUCT_PACKAGES_DEBUG += \
     bootctl \
     update_engine_client
 
-# VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v32.so
-
 # Inherit from motorola msm8998-common
 TARGET_MOTO_PLATFORM := sdm660
 $(call inherit-product, device/motorola/msm8998-common/common.mk)
